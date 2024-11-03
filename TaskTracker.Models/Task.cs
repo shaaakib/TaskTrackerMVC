ff1d9f3e60bd85319608d3a31a157c4f332cad1b
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2024-11-03 15:18:10 +0600

    Add project files.

---
+++ TaskTracker.Models/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Models
+{
+    public enum TaskPriority
+    {
+        High,
+        Medium,
+        Low
+    }
+
+    public enum TaskStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public TaskPriority Priority { get; set; }
+        [Required]
+        public TaskStatus Status { get; set; }
+    }
+}
